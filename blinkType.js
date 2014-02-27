1511ab476c794a111eaf2e8bdc759a091f3810b9
aokabin <mi.aoka.breakdance@gmail.com>
2014-02-28 00:19:00 +0900

    blink puttern 光るパターンをled.frequencyR=10という感じにした

--- blinkType.js
+++ blinkType.js
@@ -6,38 +6,47 @@ $(
 	var checkLocate = (function() {
 		window.global.meter = getDistance(window.global.rat, window.global.lon);
 
-		if (window.global.meter >= 10) {
-			led = new VincleLed(100, 0.1);
+		led = new VincleLed(100, 0.1);
+		led.frequencyL = 100;
+
+		if (window.global.meter <= 1) {
+			led.frequencyR = 100;
 		};
-		else if (window.global.meter < 10) {
-			led = new VincleLed(100, 0.5);
-		};
-		else if (window.global.meter < 9) {
-			led = new VincleLed(100, 1);
-		};
-		else if (window.global.meter < 8) {
-			led = new VincleLed(100, 2);
-		};
-		else if (window.global.meter < 7) {
-			led = new VincleLed(100, 3);
-		};
-		else if (window.global.meter < 6) {
-			led = new VincleLed(100, 4);
-		};
-		else if (window.global.meter < 5) {
-			led = new VincleLed(100, 5);
-		};
-		else if (window.global.meter < 4) {
-			led = new VincleLed(100, 6);
-		};
-		else if (window.global.meter < 3) {
-			led = new VincleLed(100, 8);
-		};
+
 		else if (window.global.meter < 2) {
-			led = new VincleLed(100, 10);
+			led.frequencyR = 10
 		};
+
+		else if (window.global.meter < 3) {
+			led.frequencyR =8;
+		};
+
+		else if (window.global.meter < 4) {
+			led.frequencyR =6;
+		};
+
+		else if (window.global.meter < 5) {
+			led.frequencyR =5;
+		};
+
+		else if (window.global.meter < 6) {
+			led.frequencyR =4;
+		};
+
+		else if (window.global.meter < 7) {
+			led.frequencyR =3;
+		};
+
+		else if (window.global.meter < 8) {
+			led.frequencyR =1;
+		};
+
+		else if (window.global.meter < 9) {
+			led.frequencyR =0.5;
+		};
+
 		else {
-			led = new VincleLed(100, 100);
+			led.frequencyR = 0.1;
 		};
 
 		led.on();
